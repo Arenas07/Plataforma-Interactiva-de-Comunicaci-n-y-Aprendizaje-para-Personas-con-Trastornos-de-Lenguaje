8559dc7de2e6840efd95f1fd39da22d576256ab3
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-09-29 19:53:07 -0500

    feat: :sparkles: Implementacion de rutas api con lecciones

--- Communication-api/routes/api.php
+++ Communication-api/routes/api.php
@@ -5,4 +5,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+use App\Http\Controllers\LeccionController;
+
+Route::apiResources([
+    'lecciones' => LeccionController::class,
+]);
+
+Route::post('/lecciones/{id}/tarjetas', [LeccionController::class, 'addTarjeta']);
+Route::delete('/lecciones/{id}/tarjetas/{tarjetaId}', [LeccionController::class, 'removeTarjeta']);
+
 Route::apiResource('metodo', MetodoComunicacionController::class);
+
